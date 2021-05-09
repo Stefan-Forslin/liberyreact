03cd159836f1bc6622620b16d0be4fb419beb7ee
Stefan Forslin <stefan.forslin@iths.se>
2021-05-09 16:54:10 +0200

    first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	  <main>
+	  <div className="border">
+		 <h1>Catalouge</h1>
+			 <div className="flex-container">
+			    <div className="book-card">
+			      <h2>titel</h2>
+			      <p>författare</p>
+			     <button>borrow me!</button>
+				 </div>
+			  </div>
+			 </div>
+			  <div className="border">
+			    <h1> My borrowed books</h1>
+			     <div className="flex-container">
+			      <div className="book-card">
+			      <h2>titel</h2>
+			      <p>författare</p>
+			     <button>Return</button>
+			    </div>
+	         </div>
+	    </div>
+		</main>
   );
 }
 
